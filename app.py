d7054f336c12e3967364103dc26e8cd83e63b5e4
robelMike <robelmichael102@gmail.com>
2020-02-15 16:25:52 +0100

    little change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 import Adafruit_DHT
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
